fe07c0452ba80930540d72972a6ab77b7023dfa9
sujith kumar <148097955+sujith52@users.noreply.github.com>
2025-07-10 17:08:53 +0530

    add

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace 'your_password' with your actual PostgreSQL password
-DATABASE_URL = "postgresql://postgres:postgres@localhost/recommend_db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
